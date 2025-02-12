150993ff64cc544e7629c155a1110631ba982b59
Cristian Luna <cluna-2023308@kinal.edu.gt>
2025-02-12 09:23:19 -0600

    build: inicio de proyecto

---
+++ configs/server.js
@@ -0,0 +1,23 @@
+"use strict"
+
+import express from "express"
+import { dbConnection } from "./mongo.js"
+
+const conectarDB = async () =>{
+    try{
+        await dbConnection()
+    }catch(err){
+        console.log(`Database connection failed: ${err}`)
+    }
+}
+
+export const initServer = () => {
+    const app = express()
+    try{
+        conectarDB()
+        app.listen(process.env.PORT)
+        console.log(`Server running on port ${process.env.PORT}`)
+    }catch(err){
+        console.log(`Server init failed: ${err}`)
+    }
+}
