e3ad37275d71c51418938f948d0a13348cdbfc31
Informatica <informatica@alumno.local>
2025-02-12 17:20:57 -0600

    primer entrega

--- configs/server.js
+++ configs/server.js
@@ -1,7 +1,28 @@
 "use strict"
 
 import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import morgam from "morgan"
 import { dbConnection } from "./mongo.js"
+import authRoutes from "../src/auth/auth.routes.js"
+import userRoutes from "../src/user/user.routes.js"
+import apiLimiter from "../src/middlewares/rate-limit-validator.js"
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgam("dev"))
+    app.use(apiLimiter)
+}
+
+const routes = (app) =>{
+    app.use("/saleSystem/v1/auth", authRoutes)
+    app.use("/saleSystem/v1/user", userRoutes)
+}
+
 
 const conectarDB = async () =>{
     try{
@@ -14,7 +35,9 @@ const conectarDB = async () =>{
 export const initServer = () => {
     const app = express()
     try{
+        middlewares(app)
         conectarDB()
+        routes(app)
         app.listen(process.env.PORT)
         console.log(`Server running on port ${process.env.PORT}`)
     }catch(err){
